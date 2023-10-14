dbf1f4c180f4266dd55dbd3d39b05b124ae9b2fd
Sophie Delice <delicesophie824@gmail.com>
2023-10-14 12:55:57 -0400

    wips: file system

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-const addition = document.querySelector("#add"); 
+
 const numberOne = document.querySelector("#number1"); 
 const numberTwo = document.querySelector("#number2"); 
 const result = document.querySelector("#result");
 
+
+const addition = document.querySelector("#add"); 
 addition.onclick = () => {
     result.innerHTML = Number(numberOne.value) + Number(numberTwo.value);
   }
@@ -11,19 +13,17 @@ addition.onclick = () => {
 
 const substraction= document.querySelector("#substract"); 
  substraction.onclick = () => {
-    result.innerHTML = Number(numberOne.value) + Number(numberTwo.value); 
+    result.innerHTML = Number(numberOne.value) -  Number(numberTwo.value); 
  }
 
 
-
-
 const multiplication = document.querySelector("#multiply"); 
-
-
-
-
-
-
-
+multiplication.onclick = () => {
+    result.innerHTML = Number(numberOne.value) * Number(numberTwo.value); 
+    
+}
 
 const division = document.querySelector("#divide");
+division.onclick = () => {
+    result.innerHTML = Number(numberOne.value) /  Number(numberTwo.value); 
+}
