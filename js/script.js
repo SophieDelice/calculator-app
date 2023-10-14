409ab9f5372d9d9e627124de7c8a87c5df867b0e
Sophie Delice <delicesophie824@gmail.com>
2023-10-14 12:33:25 -0400

    wips: adds filesystems

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const addition = document.querySelector("#add"); 
+const numberOne = document.querySelector("#number1"); 
+const numberTwo = document.querySelector("#number2"); 
+const result = document.querySelector("#result");
+
+addition.onclick = () => {
+    result.innerHTML = Number(numberOne.value) + Number(numberTwo.value);
+  }
+
+
+
+const substraction= document.querySelector("#substract"); 
+ substraction.onclick = () => {
+    result.innerHTML = Number(numberOne.value) + Number(numberTwo.value); 
+ }
+
+
+
+
+const multiplication = document.querySelector("#multiply"); 
+
+
+
+
+
+
+
+
+const division = document.querySelector("#divide");
